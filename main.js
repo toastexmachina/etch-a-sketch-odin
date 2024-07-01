8367183319c7614b38d73e4219e15b3ebc16ba68
toastexmachina <156368225+toastexmachina@users.noreply.github.com>
2024-07-01 15:50:10 -0400

    Write basic pseudocode for the project

---
+++ main.js
@@ -0,0 +1,11 @@
+// 16 x 16 Grid
+
+// Target the container div in the html file
+// Create 16x16=256 div elements in JS and insert them dynamically
+// Style elements so the div elements have a border
+
+
+
+// CSS
+// 16 elements per row
+// Flexbox dynamically change as the window changes
