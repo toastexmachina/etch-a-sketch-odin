01fca52b1fb7905c62511733f4d2635e942e1b02
toastexmachina <156368225+toastexmachina@users.noreply.github.com>
2024-07-01 16:23:04 -0400

    Create 256 divs using JS to be appended to the HTML container div
    These divs will make up the grid cells for the
    etch-a-sketch.

--- main.js
+++ main.js
@@ -4,6 +4,17 @@
 // Create 16x16=256 div elements in JS and insert them dynamically
 // Style elements so the div elements have a border
 
+// Store the div element in the html file to a variable
+const container = document.querySelector("#container");
+
+// Loop to create 256 div elements within the container div
+for (let i=0; i<256; i++) {
+    const newDiv = document.createElement("div");
+    container.appendChild(newDiv);
+    newDiv.textContent = `${i}`;
+}
+
+
 
 
 // CSS
