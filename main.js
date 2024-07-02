1a1c02d091f8d2e93d6d2bb00be769c93f0386d9
toastexmachina <156368225+toastexmachina@users.noreply.github.com>
2024-07-02 13:50:33 -0400

    Add button that creates a popup after being clicked
    So users can enter the grid size.

--- main.js
+++ main.js
@@ -1,6 +1,20 @@
 // Store the div element in the html file to a variable
 const container = document.querySelector("#container");
 
+// Button at the top of the screen
+// Button click triggers a popup - "How many squares per side?"
+// The number entered removes the existing grid and creates a new grid with the correct number of squares
+    // Change the flex-basis property to js
+
+const button = document.createElement("button");
+document.body.appendChild(button);
+button.textContent = "Click Here To Begin";
+
+button.addEventListener("click", function(e){
+    let gridSize = prompt("How many boxes would you like?", "16x16");
+});
+
+
 // Loop to create 256 div elements within the container div
 for (let i=0; i<256; i++) {
     const newDiv = document.createElement("div");
@@ -10,8 +24,4 @@ for (let i=0; i<256; i++) {
     newDiv.addEventListener("mouseover", function(e){
         e.target.style.backgroundColor = "black";
     });
-}
-
-// hover element to change div color when mouse passes over
-// eventListener - mouse hover
-    // function to change background color
+};
